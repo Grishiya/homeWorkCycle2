b092c2e414c23229e32416b9288446b9098ec62a
Grisha <guruwolf8@gmail.com>
2023-06-01 15:02:10 +0300

    Задача 3.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         task1();
         task2();
-
+        task3();
     }
-    public static void task1(){
+
+    public static void task1() {
         System.out.println("Задача 1.");
         int money = 0;
         int month = 0;
-        int postponed=15000;
+        int postponed = 15000;
         for (; money <= 2_459_000; month++) {
 
             money = money + postponed;
@@ -16,6 +17,7 @@ public class Main {
         }
 
     }
+
     public static void task2() {
         System.out.println("Задача 2.");
         int a = 1;
@@ -30,4 +32,18 @@ public class Main {
         System.out.println();
     }
 
+    public static void task3() {
+        int contryY = 12_000_000;
+        int birthRate = 17;
+        int mortality = 8;
+        int lifePeopleOn1000 = birthRate - mortality;
+        int thousandsPeopleCountryY = contryY / 1000;
+        int totalLifePeopleYear = thousandsPeopleCountryY * lifePeopleOn1000;
+
+        for (int i = 1; i <= 10; i++) {
+            contryY = contryY + totalLifePeopleYear;
+            System.out.println("Год " + i + " численность населения составляет " + contryY);
+        }
+        System.out.println( );
+    }
 }
